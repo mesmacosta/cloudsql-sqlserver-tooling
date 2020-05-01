391f28cd1918f5c99101e2322afb8a500e27a8e0
mesmacosta <mesmacosta@gmail.com>
2020-05-01 15:10:43 -0300

    Update docker image

--- init-db.sh
+++ init-db.sh
@@ -56,7 +56,7 @@ echo -e "\033[1;42m [STEP 3] POPULATE DATABASE \033[0m"
 
 # Generate Metadata
 docker run --rm --tty \
-#ADD DOCKER IMAGE:stable  \
+mesmacosta/sqlserver-metadata-generator:stable  \
 --sqlserver-host=$public_ip_address \
 --sqlserver-user=$username \
 --sqlserver-pass=$password \
