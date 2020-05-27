0788e1daed1620f1d2096b5c27cafcb822177f3f
Marcelo Costa <mesmacosta@gmail.com>
2020-05-27 19:35:59 -0300

    Update init-db.sh

--- init-db.sh
+++ init-db.sh
@@ -19,7 +19,7 @@ EOF
 
 echo -e "\033[1;42m [STEP 1] Enable required APIs \033[0m"
 
-gcloud services enable sqladmin.googleapis.com  --project $project
+gcloud services enable datacatalog.googleapis.com sqladmin.googleapis.com --project $project
 
 echo -e "\033[1;42m [STEP 2] Create Cloud SQL - SQLServer environment \033[0m"
 # download utility tootls directly into ~/
